49056305aef4ce2fdd4b6ca0e7e075110554c9fc
Dinq <quangtruongdit@gmail.com>
2023-10-20 16:56:43 +0700

    Add Product Service

---
+++ MSA.ProductService/Program.cs
@@ -0,0 +1,34 @@
+using MSA.ProductService.Entities;
+using MSA.Common.MongoDB;
+using MSA.Common.Contracts.Domain;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddMongo().AddRepositories<Product>("product");
+
+builder.Services.AddControllers(options => {
+    options.SuppressAsyncSuffixInActionNames = false;
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
